ca5f5f339d7c7af97b44a4cbeebda4239aca513f
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2024-08-23 00:58:15 -0400

    added skills section still need to personalize

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Banner from './components/Banner';
+import { Skills } from './components/Skills';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <NavBar/>
+      <Banner/>
+      <Skills/>
     </div>
   );
 }
--- personal-website/src/components/NavBar.js
+++ personal-website/src/components/NavBar.js
@@ -3,7 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {useState,useEffect} from 'react'
-function Header() 
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import githubIcon from '../assets/img/github_icon.png'
+
+
+function NavBar() 
 {
 
   const [activeLink, setActiveLink] = useState("home");
@@ -31,7 +38,7 @@ function Header()
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">
-          <img src = "" alt = "Jerry Xiao"></img>
+          <img src = {logo} alt = "Jerry Xiao"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -39,7 +46,7 @@ function Header()
             <Nav.Link href="#home" className = {activeLink==="home"? "active navbar-link":"navbar-link"} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills"className = {activeLink==="skills"? "active navbar-link":"navbar-link"} onClick={()=>onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href = "#projects"className = {activeLink==="projects"? "active navbar-link":"navbar-link"} onClick={()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            <NavDropdown title="About Me!" id="basic-nav-dropdown">
+            <NavDropdown title="About Me!" id="basic-nav-dropdown" className = "about-dropdown">
               <NavDropdown.Item href="#action/3.1">Competitive Programming</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Education
@@ -53,7 +60,9 @@ function Header()
           </Nav>
           <span className = "navbar-text">
             <div className = "socials">
-              <a href = "#">img src = {} alt = ""</a>
+            <a href="#" className = "github-icon"><img src={githubIcon} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className ="contact-button" onClick={()=>console.log("implement connect")}>
               <span>Connect with me!</span>
@@ -66,4 +75,6 @@ function Header()
   );
 }
 
-export default Header;
+export default NavBar;
+
+
