f004230fd0edb4d7a96e613803205229a0b39556
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2025-08-15 19:43:21 -0400

    added image and first divider

--- personal_website/src/components/Home/Home.jsx
+++ personal_website/src/components/Home/Home.jsx
@@ -1,40 +1,71 @@
 import React from "react";
 
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Container} from '@mui/material'
 import {ReactTyped} from 'react-typed'
+import JerryX from "../AboutMe/images/jerryx.jpg"
 
 export default function Home(){
     
     const heroText = "Hi, I'm Jerry Xiao:\nAspiring Software \nEngineer, Student,\n and Problem Solver!";
     
     return (
-        <Box
-            sx = {{
-                mt:10,
-                display:'flex',
-                backgroundColor: 'black',
-                width: '100%',
-                //border:1,
-                //borderColor: 'white'
-            }}
-        >
-            {/*Hero section */}
-            <Typography
-                variant = 'h2'
-                sx = {{
-                    textTransform: 'uppercase',
-                    color: 'white',
-                    whiteSpace: 'pre-line',
+        <Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between", // text left, image right
+                    mt: 5,
+                    width: "100%",
+
                 }}
             >
-                <ReactTyped
-                strings={[heroText]}
-                typeSpeed={50}
-                backSpeed={0}  // do not erase if you want lines to stay
-                showCursor={true}
-                loop
+                {/* Typing text */}
+                <Box sx={{ flex: 1, mr: 4, height: "70vh", }}> {/* reserve space for text */}
+                    <Typography
+                    variant="h2"
+                    sx={{
+                        textTransform: "uppercase",
+                        color: "white",
+                        whiteSpace: "pre-line",
+                        ml:10,
+                        mt:8
+                    }}
+                    >
+                    <ReactTyped
+                        strings={[heroText]}
+                        typeSpeed={50}
+                        backSpeed={0}
+                        showCursor={true}
+                        loop
+                    />
+                    </Typography>
+                </Box>
+
+                {/* Image fixed to right */}
+                <Box
+                    component="img"
+                    src={JerryX}
+                    alt="Jerry"
+                    sx={{
+                        position: "absolute",
+                        right: 190,
+                        height: 375,       // adjust as needed
+                        width: "auto",
+                    }}
                 />
-            </Typography>
+            </Box>
+
+            <Box
+                sx = {{
+                    backgroundColor: 'white',
+                    height:'2px',
+                    width:'93%',
+                    mx: 'auto'
+                }}
+            />
+
+            
         </Box>
     )
 }
